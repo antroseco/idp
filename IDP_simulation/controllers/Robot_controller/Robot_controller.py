315a7934587883ac10e6b9cbfc8b308d74ca73ef
vnjaradi <valentina.njaradi@gmail.com>
2021-03-06 03:19:06 +0100

    Rewritten code, added class Robot()
    Haven't fully finished this, in the Robot() class are basically all initialisations and functions that don't use robot.step. In searchCalculations are all utilFunctions that sweep uses to find the boxes

--- IDP_simulation/controllers/Robot_controller/Robot_controller.py
+++ IDP_simulation/controllers/Robot_controller/Robot_controller.py
@@ -1,132 +1,14 @@
 """Robot_controller controller."""
-from controller import Robot
-from controller import Motor
-from controller import DistanceSensor
-from controller import Emitter
-from controller import Receiver
+import controller
+from robot import Robot
+from searchCalculations import *
 import numpy as np
 import math
 import time
-from matplotlib import pyplot as plt
 
 TIME_STEP = 64
 COMMUNICATION_CHANNEL = 1
 MAX_VELOCITY = 6
-
-
-
-def setup_infrared():
-    infrared = robot.getDevice('IR Sensor')
-    infrared.enable(TIME_STEP)
-    return infrared
-
-
-def setup_ultrasonic():
-    dsUltrasonic = robot.getDevice('ultrasonic')
-    dsUltrasonic.enable(TIME_STEP)
-    return dsUltrasonic
-
-
-def setup_wheels():
-    """
-    sets up wheels
-    input:/
-    return: left, right wheel (type Motor)
-    """
-    left_wheel = robot.getDevice('left_wheel')
-    left_wheel.setPosition(float('inf'))
-    left_wheel.setVelocity(0.0)
-    
-    right_wheel = robot.getDevice('right_wheel')
-    right_wheel.setPosition(float('inf'))
-    right_wheel.setVelocity(0.0)
-    
-    return left_wheel, right_wheel
-
-def setup_claw(servoName,sensorName):
-    """
-    sets up claw servo and accompanying position sensor
-    input:name of the RotationalMotor that acts as servo, name of the PositionSensor accompanying that Servo
-    return: claw (type RotationalMotor), claw_sensor (type PositionSensor)
-    """
-    claw = robot.getDevice(servoName)
-    claw.setPosition(0.0)
-    claw_sensor = robot.getDevice(sensorName)
-    claw_sensor.enable(TIME_STEP)
-    return claw, claw_sensor
-    
-def setup_lightsensor(sensorName):
-    """
-    sets up lightSensor
-    input:name of the LightSensor default TETP4400
-    return: lightSensor (type LightSensor)
-    """
-    lightSensor = robot.getDevice(sensorName)
-    lightSensor.enable(TIME_STEP)
-    return lightSensor
-
-def setup_communication():
-    """
-    sets up communication devices
-    input:/
-    return: emitter (type Emitter) and receiver (type Receiver)
-    """
-        
-    emitter = robot.getDevice('emitter')
-    emitter.setChannel(COMMUNICATION_CHANNEL)
-
-    receiver = robot.getDevice('receiver')
-    receiver.setChannel(COMMUNICATION_CHANNEL)
-    receiver.enable(TIME_STEP)
-    
-    return emitter, receiver
-
-
-def setup_sensors():
-    """
-    sets up all sensors
-    input: /
-    return: gps, compass
-    """
-    gps = robot.getDevice('gps')
-    gps.enable(TIME_STEP)
-    
-    compass = robot.getDevice('compass')
-    compass.enable(TIME_STEP)
-    
-    compass1 = robot.getDevice('compass1')
-    compass1.enable(TIME_STEP)
-    
-    return gps, compass, compass1
-    
-
-
-def send_message(message):
-    """
-    sends string message through a receiver
-    input: string message
-    return: /
-    """
-
-    data = message.encode('utf-8')
-    emitter.send(data)
-    
-    
-def get_message():
-    """
-    gets the first message in receiver's queue and pops it from queue
-    input: /
-    return: string message, if there aren't any messages returns ""
-    """
-    
-    if receiver.getQueueLength() > 0:
-        data = receiver.getData()
-        message = data.decode('utf-8')
-        receiver.nextPacket()
-        return message
-    
-    return ""
-
 
 
 def bearing1(compass_obj): 
@@ -166,7 +48,7 @@ def PID_rotation(coord):
     
     
     for i in range(2):
-        location = gps.getValues()
+        location = robot.gps.getValues()
         required = np.arctan2((coord[0]-location[0]),(-(coord[1]-location[2])))
         robot.step(TIME_STEP)
       
@@ -195,20 +77,12 @@ def PID_rotation(coord):
         required = -np.arctan2(-(coord[0]-location[0]),(-(coord[1]-location[2])))
         required = required *180.0/np.pi 
         #print('Q4')
-            
-    
-    
-    
-    error = required - bearing1(compass)
-   
 
-    
-    
+    error = required - bearing1(robot.compass)
+
     previous_error = error
     
     final_error = 0.5
-        
-        
  
     while abs(error) > final_error:
         #print(required,'required')
@@ -225,10 +99,10 @@ def PID_rotation(coord):
         elif v < -6.0:
             v = -6.0
     
-        left_wheel.setVelocity(-v)
-        right_wheel.setVelocity(v)
+        robot.left_wheel.setVelocity(-v)
+        robot.right_wheel.setVelocity(v)
         previous_error = error
-        error = required - bearing1(compass)
+        error = required - bearing1(robot.compass)
         robot.step(TIME_STEP)
     
     return
@@ -239,7 +113,7 @@ def PID_rotation(coord):
 
 
 def PID_translation(coord):
-    error = ((coord[0] - gps.getValues()[0])**2 +(coord[1] - gps.getValues()[2])**2)**(1/2)
+    error = ((coord[0] - robot.gps.getValues()[0])**2 +(coord[1] - robot.gps.getValues()[2])**2)**(1/2)
     
     final_error = 0.15
     
@@ -255,12 +129,12 @@ def PID_translation(coord):
                 
                 
             
-            left_wheel.setVelocity(v)
-            right_wheel.setVelocity(v)
+            robot.left_wheel.setVelocity(v)
+            robot.right_wheel.setVelocity(v)
             
         robot.step(TIME_STEP)
-        x = (coord[0] - gps.getValues()[0])
-        z = (coord[1] - gps.getValues()[2])
+        x = (coord[0] - robot.gps.getValues()[0])
+        z = (coord[1] - robot.gps.getValues()[2])
         
         previous_error = error
         error = (x**2 + z**2)**0.5
@@ -268,61 +142,12 @@ def PID_translation(coord):
         if previous_error < error:
             PID_rotation(coord)
         
-    left_wheel.setVelocity(0)
-    right_wheel.setVelocity(0)    
+    robot.left_wheel.setVelocity(0)
+    robot.right_wheel.setVelocity(0)    
         
     return
     
-    
-    
-    
-def get_wall_position(angle):
-    """
-    finds distance from centre of the robot to the wall depending on the position and rotation of the robot
-    input: angle (bearing 0 to 360 degrees)
-    """    
-    position = gps.getValues()
-    x = position[0]
-    z = position[2]
-    
-    
-    #find angles at which you can see the walls
-    #eg top wall can be seen at angles between left_wall and top_wall
-    #eg right wall can be seen at angles between top_wall and right_wall
-    
-    left_wall = math.degrees(math.atan(abs((1.2 - z) / (1.2 - x))))
-    top_wall = math.degrees(math.atan(abs((1.2 - z) / (-1.2 - x)))) + 90
-    right_wall = math.degrees(math.atan(abs((-1.2 - z) / (-1.2 - x)))) + 180
-    bottom_wall = math.degrees(math.atan(abs((-1.2 - z) / (1.2 - x)))) + 270
-    
-    #now find the distance from the wall that robot is looking at
-    dist = 0
-    
-    if bottom_wall < angle or angle <= left_wall:
-        dist = abs((1.2 - x)/math.cos(math.radians(angle)))
-    elif angle <= top_wall:
-        dist = abs((1.2 - z)/math.sin(math.radians(angle)))
-    elif angle <= right_wall:
-        dist = abs((-1.2 - x)/math.cos(math.radians(angle)))
-    else:
-        dist = abs((-1.2 - z)/math.sin(math.radians(angle)))
-    
-    return dist
-    
-    
-    
-    
-def potential_box_position(distance, angle, position):
-    """
-    finds the box position based on the current angle and location of the robot
-    and the distance between the robot and box
-    """    
-    x = position[0] + distance * math.cos(math.radians(angle))
-    z = position[2] + distance * math.sin(math.radians(angle))
-    
-    return x, z
-    
-    
+
     
     
 
@@ -335,7 +160,7 @@ def sweep(velocity = 0.5):
     
     
     #find current rotation [0-360 degrees]
-    initial_angle = bearing(compass1)   
+    initial_angle = bearing(robot.compass1)   
     
     #store potential boxes locations     
     boxes = []    
@@ -345,27 +170,31 @@ def sweep(velocity = 0.5):
     
     while swept_angle < 350:
         
-        right_wheel.setVelocity(velocity)
-        left_wheel.setVelocity(-velocity)
+        robot.right_wheel.setVelocity(velocity)
+        robot.left_wheel.setVelocity(-velocity)
         
         robot.step(TIME_STEP)
         
-        
-        #get infrared reading and convert to meters
-        infrared_dist = 0.7611 * math.pow(infrared.getValue(), -0.9313) - 0.1252
-        
         #distance from robot centre to wall in this direction
-        current_angle = bearing(compass1)
-        wall_dist = get_wall_position(current_angle)
+        current_angle = bearing(robot.compass1)
+        current_position = robot.gps.getValues()
+        wall_dist = get_wall_position(current_angle, current_position)
         
         #wall_dist is decreased by robot-sensor distance
         wall_dist -= 0.11
         
+          
+        #get infrared reading and convert to meters
+        infrared_dist = 0.7611 * math.pow(robot.infrared.getValue(), -0.9313) - 0.1252
+ 
+        
+        #print(infrared_dist, wall_dist)
+        
         
         #if measured distance is less than wall_dist then assume there's a box
-        #also if wall is more than 1.5 away disregard measurements because it's further than sensor's range
-        if abs(wall_dist - infrared_dist) > 0.06 and wall_dist < 1.5:
-            x, z = potential_box_position(infrared_dist + 0.11, current_angle, gps.getValues())
+        #also if wall is more than 1.5 away disregard measurements because it's further than sensor's range   
+        if abs(wall_dist - infrared_dist) > 0.1 and wall_dist < 1.4:
+            x, z = potential_box_position(infrared_dist + 0.11, current_angle, current_position)
             boxes.append([x, z])
         
                 
@@ -374,57 +203,16 @@ def sweep(velocity = 0.5):
         else:
             swept_angle = 360 - initial_angle + current_angle
             
-        
+    locations = box_position(np.array(boxes))     
             
-    right_wheel.setVelocity(0)
-    left_wheel.setVelocity(0)
+    robot.right_wheel.setVelocity(0)
+    robot.left_wheel.setVelocity(0)
     robot.step(TIME_STEP)
     
-    return np.array(boxes)
+    return locations
 
-
-
-def box_position(potential_boxes):
-    """
-    we will consecutive positions in array that all come from the same box
-    find which belong to the same box and average them out
-    input: an array with potential box locations
-    returns: array of approximated box positions
-    """
-    
-    
-    locations = []
-    
-    same_box_num = 1
-    x_avg = potential_boxes[0][0]
-    z_avg = potential_boxes[0][1]
-    
-    
-    for i in range(1, boxes.shape[0]):
-    
-        change = math.sqrt((potential_boxes[i][0] - potential_boxes[i-1][0])**2 + (potential_boxes[i][1] - potential_boxes[i-1][1])**2)
-        
-        if change < 0.2:
-            x_avg += potential_boxes[i][0]
-            z_avg += potential_boxes[i][1]
-            same_box_num += 1
-        
-        else:
-            x_avg = x_avg / same_box_num
-            z_avg = z_avg / same_box_num
-            locations.append([x_avg, z_avg])
-            
-            x_avg = potential_boxes[i][0]
-            z_avg = potential_boxes[i][1]
-            same_box_num = 1
-            
-            
-    x_avg = x_avg / same_box_num
-    z_avg = z_avg / same_box_num
-    locations.append([x_avg, z_avg])
-    
-    return np.array(locations)
                 
+
 def set_claw(targetAngle, targetClaw, targetSensor):
     """move the box_claw to the input angle, 
     input in degrees, use positionSensor to provide feedback
@@ -445,10 +233,10 @@ def set_claw(targetAngle, targetClaw, targetSensor):
     # box_claw.setPosition(np.pi/2)
     
 def withdraw_boxclaw():
-    set_claw(90,box_claw,box_claw_sensor)
+    set_claw(90, box_claw, box_claw_sensor)
     
 def deploy_boxclaw():
-    set_claw(0,box_claw,box_claw_sensor)
+    set_claw(0, box_claw, box_claw_sensor)
 
 def set_dualclaw(targetAngle,targetClaw1,targetSensor1,targetClaw2,targetSensor2):
     desired = targetAngle*np.pi/180
@@ -469,6 +257,7 @@ def set_dualclaw(targetAngle,targetClaw1,targetSensor1,targetClaw2,targetSensor2
             break
         previous = measurement
         robot.step(TIME_STEP)
+    
         
 def deploy_dualclaw(targetClaw1,targetSensor1,targetClaw2,targetSensor2):
     """function to make the dual-claw go from open to closed, detect colour of the block it holds in this process
@@ -485,8 +274,8 @@ def deploy_dualclaw(targetClaw1,targetSensor1,targetClaw2,targetSensor2):
     detectLowerBound = 41 #(environment is 40),one reading above this value turns red or green to True
     
     while error > accuracy:
-        redValue = measureLight()[0]
-        greenValue = measureLight()[1]
+        redValue = robot.measureLight()[0]
+        greenValue = robot.measureLight()[1]
         if redValue > detectLowerBound:
             red = True
         if greenValue > detectLowerBound:
@@ -534,39 +323,44 @@ def withdraw_dualclaw(targetClaw1,targetSensor1,targetClaw2,targetSensor2):
         error = abs(desired - targetSensor1.getValue())
         
 
-def measureLight():
-    # print(lightsensorRed.getValue(),lightsensorGreen.getValue())
-    return [lightsensorRed.getValue(),lightsensorGreen.getValue()]
 
-robot = Robot()
 
-left_wheel, right_wheel = setup_wheels()
-emitter, receiver = setup_communication()
-gps, compass, compass1 = setup_sensors()
-dsUltrasonic = setup_ultrasonic()
-infrared = setup_infrared()
-box_claw, box_claw_sensor = setup_claw('box_claw','box_claw_sensor')
-left_claw, left_claw_sensor = setup_claw('left_claw','left_claw_sensor')
-right_claw, right_claw_sensor = setup_claw('right_claw','right_claw_sensor')
-lightsensorRed = setup_lightsensor('TEPT4400_RED')
-lightsensorGreen = setup_lightsensor('TEPT4400_GREEN')
-
+robot = Robot(controller.Robot())
 
 robot.step(TIME_STEP)
 
-withdraw_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
+positions = sweep(0.6)
+print(positions)
 
-PID_translation((0.3,0.7))
+for pos in positions:
+    
+    withdraw_dualclaw(robot.left_claw, robot.left_claw_sensor, robot.right_claw, robot.right_claw_sensor)
+    
+    PID_translation(pos)
+    deploy_dualclaw(robot.left_claw, robot.left_claw_sensor, robot.right_claw, robot.right_claw_sensor)
+    
+    robot.step(TIME_STEP)
+    PID_translation((0.9, -0.9))
+    withdraw_dualclaw(robot.left_claw, robot.left_claw_sensor, robot.right_claw, robot.right_claw_sensor)
+    
+    robot.step(TIME_STEP)
+    PID_translation((0, 0))
+    
 
-deploy_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
-robot.step(TIME_STEP)
-withdraw_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
+    
 
-PID_translation((0.3,0))
-deploy_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
+#withdraw_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
+
+#PID_translation((0.3,0.7))
+
+#deploy_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
+#robot.step(TIME_STEP)
+#withdraw_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
+
+#PID_translation((0.3,0))
+#deploy_dualclaw(left_claw,left_claw_sensor,right_claw,right_claw_sensor)
 
 
-print('end')
 while robot.step(TIME_STEP) != -1:
     pass
-# print('end')
+print('end')
---
+++ IDP_simulation/controllers/Robot_controller/robot.py
@@ -0,0 +1,123 @@
+import controller
+import numpy as np
+
+
+class Robot:
+    
+    TIME_STEP = 64
+    COMMUNICATION_CHANNEL = 1
+    MAX_VELOCITY = 6
+
+    left_wheel_name = 'left_wheel'
+    right_wheel_name = 'right_wheel'
+    box_claw_name = 'box_claw'
+    box_claw_sensor_name = 'box_claw_sensor'
+    left_claw_name = 'left_claw'
+    left_claw_sensor_name = 'left_claw_sensor'
+    right_claw_name = 'right_claw'
+    right_claw_sensor_name = 'right_claw_sensor'
+    infrared_name = 'IR Sensor'
+    dsUltrasonic_name = 'ultrasonic'
+    lightSensorRed_name = 'TEPT4400_RED'
+    lightSensorGreen_name = 'TEPT4400_GREEN'
+    emitter_name = 'emitter'
+    receiver_name = 'receiver'
+    gps_name = 'gps'
+    compass_name = 'compass'
+    compass1_name = 'compass1'
+
+    
+    
+    def __init__(self, robot: controller.Robot, colour = 'red'):
+        """
+        initialize robot and its components, colour can be blue or red
+        
+        """
+        self._robot = robot
+        self.colour = colour #blue or green
+        
+        self.left_wheel = robot.getDevice(Robot.left_wheel_name)
+        self.right_wheel = robot.getDevice(Robot.right_wheel_name)
+        self.box_claw = robot.getDevice(Robot.box_claw_name)
+        self.left_claw = robot.getDevice(Robot.left_claw_name)
+        self.right_claw = robot.getDevice(Robot.right_claw_name)
+
+        self.box_claw_sensor = robot.getDevice(Robot.box_claw_sensor_name)
+        self.left_claw_sensor = robot.getDevice(Robot.left_claw_sensor_name)
+        self.right_claw_sensor = robot.getDevice(Robot.right_claw_sensor_name)
+
+        self.infrared = robot.getDevice(Robot.infrared_name)
+        self.dsUltrasonic = robot.getDevice(Robot.dsUltrasonic_name)
+        self.lightsensorRed = robot.getDevice(Robot.lightSensorRed_name)
+        self.lightsensorGreen = robot.getDevice(Robot.lightSensorGreen_name)
+        self.emitter = robot.getDevice(Robot.emitter_name)
+        self.receiver = robot.getDevice(Robot.receiver_name)
+        self.gps = robot.getDevice(Robot.gps_name)
+        self.compass = robot.getDevice(Robot.compass_name)
+        self.compass1 = robot.getDevice(Robot.compass1_name)
+                
+        TIME_STEP = Robot.TIME_STEP        
+        self.infrared.enable(TIME_STEP)
+        self.dsUltrasonic.enable(TIME_STEP)
+        self.box_claw_sensor.enable(TIME_STEP)
+        self.left_claw_sensor.enable(TIME_STEP)
+        self.right_claw_sensor.enable(TIME_STEP)
+        self.lightsensorRed.enable(TIME_STEP)
+        self.lightsensorGreen.enable(TIME_STEP)
+        self.receiver.enable(TIME_STEP)
+        self.gps.enable(TIME_STEP)
+        self.compass.enable(TIME_STEP)
+        self.compass1.enable(TIME_STEP)
+        
+        self.emitter.setChannel(Robot.COMMUNICATION_CHANNEL)
+        self.receiver.setChannel(Robot.COMMUNICATION_CHANNEL)
+        
+        self.box_claw.setPosition(0.0)
+        self.left_claw.setPosition(0.0)
+        self.right_claw.setPosition(0.0)
+        self.left_wheel.setPosition(float('inf'))
+        self.left_wheel.setVelocity(0.0)
+        self.right_wheel.setPosition(float('inf'))
+        self.right_wheel.setVelocity(0.0)
+    
+    def step(self, TIME_STEP):
+        self._robot.step(TIME_STEP)
+    
+        
+    def send_message(self, message: str):
+        """
+        sends string message through a receiver
+        input: string message
+        return: /
+        """
+        data = message.encode('utf-8')
+        self.emitter.send(data)
+        return
+    
+    
+    
+    def get_message(self):
+        """
+        gets the first message in receiver's queue and pops it from queue
+        input: /
+        return: string message, if there aren't any messages returns empty string
+        """        
+        if self.receiver.getQueueLength() > 0:
+            data = self.receiver.getData()
+            message = data.decode('utf-8')
+            self.receiver.nextPacket()
+            return message
+        
+        return ""
+        
+        
+    def measureLight(self):
+        # print(lightsensorRed.getValue(),lightsensorGreen.getValue())
+        return [self.lightsensorRed.getValue(), self.lightsensorGreen.getValue()]
+
+
+
+
+        
+
+
