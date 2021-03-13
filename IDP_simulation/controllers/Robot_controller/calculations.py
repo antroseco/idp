8b8e22c863904346850c4e5bccd68efd470fcb89
vnjaradi <valentina.njaradi@gmail.com>
2021-03-13 02:19:04 +0100

    added function that calculates obstacles on left and right of the robot

--- IDP_simulation/controllers/Robot_controller/calculations.py
+++ IDP_simulation/controllers/Robot_controller/calculations.py
@@ -3,6 +3,40 @@ import math
 import numpy as np
 
 
+
+def obstacle_distance_at_angle(position, bearing):
+    """
+    taking 3D gps position of the robot, and bearing 0-360 degrees
+    check how far is the first obstacle (wall or a field) from that position looking at that angle
+    returns distance from the nearest object (from an edge of the robot not its centre)
+    """
+    #convert angle to normal coordinate system
+    angle = (bearing - 90)%360
+
+
+    l = 2.4
+    d = np.linspace(0, l, 500)
+    cosine = math.cos(math.radians(angle))
+    sine = math.sin(math.radians(angle))
+    for i in range(500):
+        x = position[0] + d[i]*cosine
+        y = position[2] + d[i]*sine
+
+        #check for fields
+        if x >= -0.2 and x <= 0.2:
+            if (y >=-0.6 and y <= -0.2) or (y >= 0.2 and y <= 0.6):
+                return d[i] - 0.15
+
+        #check for walls
+        if x >= 1.2 or x <= -1.2 or y >= 1.2 or y <= -1.2:
+            return d[i] - 0.15
+
+
+
+   
+
+
+
 def get_distance(loc1, loc2):
 
     loc1 = np.array(loc1)
